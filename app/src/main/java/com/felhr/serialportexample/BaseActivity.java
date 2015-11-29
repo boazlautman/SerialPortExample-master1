3cf141605edcde44f58de11c668b350d8e76a5b2
Boaz Lautman <boaz@edt.local>
2015-11-29 15:04:26 +0200

    test commit

--- app/src/main/java/com/felhr/serialportexample/BaseActivity.java
+++ app/src/main/java/com/felhr/serialportexample/BaseActivity.java
@@ -13,7 +13,7 @@ public class BaseActivity  extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+       // setContentView(R.layout.activity_base);
     }
 
     @Override
